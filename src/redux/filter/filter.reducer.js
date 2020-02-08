6bad659871d82698baff3ae7a65cc351762bc129
Alvaro Farina <farinalvaro@gmail.com>
2020-02-08 03:40:33 -0300

    added new list to keep the selected filters - added bootstrap

--- src/redux/filter/filter.reducer.js
+++ src/redux/filter/filter.reducer.js
@@ -1,12 +1,27 @@
-import FilterActionTypes from './filter.types'
+import FilterActionTypes from './filter.types';
+import {
+  invertFilterIsApplied
+} from './filter.utils';
+
 
 const INITIAL_STATE = {
   allFilters: [],
-  error: null
-}
+  error: null,
+  selectedFilters: []
+};
 
 const filterReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case FilterActionTypes.INVERT_FILTER_IS_APPLIED:
+      return {
+        ...state,
+        selectedFilters: invertFilterIsApplied(state.selectedFilters, action.payload)
+      };
+    case FilterActionTypes.CLEAR_ALL_SELECTED_FILTERS:
+      return {
+        ...state,
+        selectedFilters: []
+      };
     case FilterActionTypes.FETCH_FILTERS_START:
       return {
         ...state
