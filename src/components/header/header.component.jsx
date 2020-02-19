9ddf49c8a7a183d076416949a10f580374a783ed
Martin Manzo <manzo.martin@hotmail.com>
2020-02-19 14:52:55 -0300

    removed styled component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { HeaderContainer, LogoContainer } from './header.styles';
+import './header.css'; 
 
 const Header = () => {
   return (
-    <HeaderContainer>
-      <LogoContainer to='/' className='px-3'>
+    <div className="header-container">
+      <a to='/' className='logo-container px-3'>
         <span className='d-md-none d-lg-flex text-decoration-none'>
           ¿Qué tenés para dar?
         </span>
-      </LogoContainer>
+      </a>
       <div>
         <Link to=''>Mapa</Link>
         <Link to='como-funciona'>¿Cómo funciona?</Link>
         <Link to='quienes-somos'>¿Quiénes somos?</Link>
         <Link to='contacto'>Contacto</Link>
       </div>
-    </HeaderContainer>
+    </div>
   );
 };
 
