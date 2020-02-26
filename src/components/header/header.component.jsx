4a026534577b3aecaf9ad8d39e7bafd6b8b0d0ac
Martin Manzo <manzo.martin@hotmail.com>
2020-02-26 11:37:27 -0300

    responsive navbar

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,44 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
 
 import './header.css';
 
 const Header = () => {
   const location = useLocation();
+  const [expanded, setExpanded] = useState(false);
 
   const getNavLinkClass = (path) => {
     return location.pathname === path ? ' active' : '';
   };
 
   return (
-    <div>
-      <nav class="navbar navbar-expand-md navbar-dark fixed-top header-container">
+    <>
+      <Navbar expand="md" fixed="top" className="header-container" expanded={expanded}>
         <div className="logo-container">
           <Link className="navbar-brand" to=''>
-            <img alt="QTPD Logo" class="navbar-brand-full" src="https://www.quetenesparadar.com/assets/img/brand/logo-85x85.png" width="55" height="55"></img>¿Qué tenés para dar?
+            <img alt="QTPD Logo" className="navbar-brand-full" src="https://www.quetenesparadar.com/assets/img/brand/logo-85x85.png" width="55" height="55"></img>¿Qué tenés para dar?
           </Link>
         </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class={"nav-item" + getNavLinkClass('/')}>
-              <Link className="nav-link" to=''>Mapa</Link>
+        <Navbar.Toggle aria-controls="navbarCollapse" onClick={() => setExpanded(expanded ? false : "expanded")} />
+        <Navbar.Collapse id="navbarCollapse">
+          <ul className="navbar-nav mr-auto">
+            <li className={"nav-item" + getNavLinkClass('/')}>
+              <Link className="nav-link" to='' onClick={() => setTimeout(() => { setExpanded(false) }, 150)}>Mapa</Link>
             </li>
-            <li class={"nav-item" + getNavLinkClass('/como-funciona')}>
-              <Link className="nav-link" to='como-funciona'>¿Cómo funciona?</Link>
+            <li className={"nav-item" + getNavLinkClass('/como-funciona')}>
+              <Link className="nav-link" to='como-funciona' onClick={() => setTimeout(() => { setExpanded(false) }, 150)}>¿Cómo funciona?</Link>
             </li>
-            <li class={"nav-item" + getNavLinkClass('/quienes-somos')}>
-              <Link className="nav-link" to='quienes-somos'>¿Quiénes somos?</Link>
+            <li className={"nav-item" + getNavLinkClass('/quienes-somos')}>
+              <Link className="nav-link" to='quienes-somos' onClick={() => setTimeout(() => { setExpanded(false) }, 150)}>¿Quiénes somos?</Link>
             </li>
-            <li class={"nav-item" + getNavLinkClass('/contacto')}>
-              <Link className="nav-link" to='contacto'>Contacto</Link>
+            <li className={"nav-item" + getNavLinkClass('/contacto')}>
+              <Link className="nav-link" to='contacto' onClick={() => setTimeout(() => { setExpanded(false) }, 150)}>Contacto</Link>
             </li>
           </ul>
-        </div>
-      </nav>
-    </div>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 };
 
