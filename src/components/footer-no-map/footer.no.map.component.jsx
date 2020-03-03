d9fa6fa99499160149ed865740a4c53467ec119d
Martin Manzo <manzo.martin@hotmail.com>
2020-03-03 11:23:27 -0300

    footer with gradient

--- src/components/footer-no-map/footer.no.map.component.jsx
+++ src/components/footer-no-map/footer.no.map.component.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
 import './footer.no.map.css';
-import logo from '../../assets/imgs/logo-navbar.png';
+
+import fbLogo from '../../assets/imgs/fb.png';
+import igLogo from '../../assets/imgs/ig.png';
+import twLogo from '../../assets/imgs/tw.png';
 
 const FooterNoMap = () => {
   return (
     <>
-      <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
-        <div class="container text-center">
-          <small>Copyright &copy; Your Website</small>
+      <footer id="sticky-footer-no-map" className="bg-dark container-fluid">
+        <div className="row">
+          <div className="col">
+            <div className="copyright-txt">
+              &#169; Copyright {(new Date().getFullYear())} - Todos los derechos reservados
+            </div>
+          </div>
+          <div className="col text-center">
+            <a href="https://www.facebook.com/quetenesparadar" target="_blank">
+              <img className="social-link-logo" alt="FB Logo" src={fbLogo} />
+            </a>
+            <a href="https://twitter.com/quetenesparadar" target="_blank">
+              <img className="social-link-logo" alt="TW Logo" src={twLogo} />
+            </a>
+            <a href="https://www.instagram.com/quetenesparadar/" target="_blank">
+              <img className="social-link-logo" alt="IG Logo" src={igLogo} />
+            </a>
+          </div>
         </div>
       </footer>
     </>
