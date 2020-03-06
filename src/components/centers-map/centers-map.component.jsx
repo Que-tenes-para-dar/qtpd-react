29cae26373535f7ee723a2c4f0596770781b6869
Martin Manzo <manzo.martin@hotmail.com>
2020-03-06 11:20:20 -0300

    added map and secrest settings file example

--- src/components/centers-map/centers-map.component.jsx
+++ src/components/centers-map/centers-map.component.jsx
@@ -1,6 +1,8 @@
+import GoogleMapReact from 'google-map-react';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import secretSettings from '../../secrets';
 import { fetchCentersFilteredStart } from '../../redux/center/center.actions';
 import { selectCenters } from '../../redux/center/center.selectors';
 import FooterMap from '../footer-map/footer.map.component';
@@ -13,13 +15,29 @@ const CentersMap = ({ centers, fetchCentersFilteredStart }) => {
     fetchCentersFilteredStart()
   }, [fetchCentersFilteredStart]);
 
+  const defaultMapProps = {
+    center: {
+      lat: -34.9032784,
+      lng: -56.1881599
+    },
+    zoom: 11
+  };
+
   return (
     <>
       <h4>Lista de centros: </h4>
       {
         centers.map(c => <p key={c._id}>{c.name}</p>)
       }
-      <FooterMap />
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: secretSettings.googleMapsApiKey }}
+          defaultCenter={defaultMapProps.center}
+          defaultZoom={defaultMapProps.zoom}
+        >
+        </GoogleMapReact>
+        <FooterMap />
+      </div>
     </>
   );
 }
