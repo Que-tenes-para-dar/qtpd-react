227dadd83bc3b520d789461c4b917411cd964293
Martin Manzo <manzo.martin@hotmail.com>
2020-03-12 10:31:44 -0300

    filters styles

--- src/redux/filter/filter.actions.js
+++ src/redux/filter/filter.actions.js
@@ -1,13 +1,20 @@
 import FilterActionTypes from './filter.types';
+import { defaultFilters } from '../../utils/constants.utils';
+
 
 export const fetchDonationTypesStart = () => ({
   type: FilterActionTypes.FETCH_DONATION_TYPES_START
 });
 
-export const fetchDonationTypesSuccess = filters => ({
-  type: FilterActionTypes.FETCH_DONATION_TYPES_SUCCESS,
-  payload: filters
-});
+export const fetchDonationTypesSuccess = (filters) => {
+  filters.forEach((filter) => {
+    defaultFilters.donationTypes.push(filter);
+  });
+  return {
+    type: FilterActionTypes.FETCH_DONATION_TYPES_SUCCESS,
+    payload: filters
+  };
+};
 
 export const fetchDonationTypesFailure = errorMessage => ({
   type: FilterActionTypes.FETCH_DONATION_TYPES_FAILURE,
