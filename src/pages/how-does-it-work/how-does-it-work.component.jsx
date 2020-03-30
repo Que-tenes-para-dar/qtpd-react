93e57169020f9c9cb8a77a272dac566588fbbeb2
Martin Manzo <manzo.martin@hotmail.com>
2020-03-30 11:08:19 -0300

    how does it work

--- src/pages/how-does-it-work/how-does-it-work.component.jsx
+++ src/pages/how-does-it-work/how-does-it-work.component.jsx
@@ -14,7 +14,7 @@ const HowDoesItWork = () => {
         <title>¿Qué tenés para dar? - ¿Cómo funciona QTPD?</title>
       </Helmet>
       <div className="page-content container-md">
-        <div className="row">
+        <div className="row pt-10">
           <div className="col">
             <h1>¿Cómo funciona QTPD?</h1>
             <hr />
@@ -36,8 +36,6 @@ const HowDoesItWork = () => {
             <hr />
           </div>
         </div>
-
-
         <div className="row">
           <div className="col-md-3 col-12 text-center">
             <img alt="Paso 1" className="img-responsive" src={step1} />
@@ -53,9 +51,58 @@ const HowDoesItWork = () => {
               una segunda vida y encontrá quién las puede necesitar.
             </p>
           </div>
+          <div className="col-12">
+            <hr />
+          </div>
         </div>
-
-
+        <div className="row">
+          <div className="col-md-3 col-12 text-center">
+            <img alt="Paso 2" className="img-responsive" src={step2} />
+          </div>
+          <div className="col-md-9 col-12">
+            <h3>Entrá a ¿Qué tenés para dar?</h3>
+            <p className="text-justify">
+              Entrá al sitio, filtrá según las cosas que hayas juntado, la distancia máxima hasta dónde lo llevarías y hacé clic en BUSCAR.
+            </p>
+            <p className="text-justify">
+              El mapa se va a actualizar y te mostrará los centros que se ajustan a tus necesidades.
+            </p>
+          </div>
+          <div className="col-12">
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-3 col-12 text-center">
+            <img alt="Paso 3" className="img-responsive" src={step3} />
+          </div>
+          <div className="col-md-9 col-12">
+            <h3>Elegí un centro</h3>
+            <p className="text-justify">
+              Una vez que tengas los resultados vas a poder ver todos los centros a los que podés ayudar.
+            </p>
+            <p className="text-justify">
+              Haciendo clic en los icónos del mapa vas a ver información sobre el centro. Vas a ver una descripción, la dirección, teléfono de contacto y horarios de atención.
+            </p>
+          </div>
+          <div className="col-12">
+            <hr />
+          </div>
+        </div>
+        <div className="row mb-20">
+          <div className="col-md-3 col-12 text-center">
+            <img alt="Paso 4" className="img-responsive" src={step4} />
+          </div>
+          <div className="col-md-9 col-12">
+            <h3>¡Hacé la donación!</h3>
+            <p className="text-justify">
+              Una vez que hayas elegido a donde quieras llevar las cosas ponte en contacto con ellos y acercaselas.
+            </p>
+            <p className="text-justify">
+              ¡Gracias por usar QTPD!
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
